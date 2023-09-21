5dcddf80040086f767d96529eb9af8f90c7703f4
chenruipeng <1259903760@qq.com>
2023-09-21 11:04:46 +0800

    feat: 视频列表静态页面

--- frontend/src/components/List/Item.vue
+++ frontend/src/components/List/Item.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="w-full h-62 bg-white">
+    <!-- 视频预览画面 -->
+    <a href="#">
+      <div class="w-full h-43.5 rounded-md bg-orange-600">
 
+      </div>
+    </a>
+
+    <!-- 底部描述 -->
+    <div class="flex">
+      <div class="w-full h-16 mt-2.5 bg-zinc-200 text-[15px] flex-1">
+        <h3 class="w-full h-11 pr-6 line-clamp-2 text-ellipsis" title="为什么现在明星网红道歉大家都不信了？为什么现在明星网红道歉大家都不信了？为什么现在明星网红道歉大家都不信了？">
+          为什么现在明星网红道歉大家都不信了？
+          为什么现在明星网红道歉大家都不信了？
+          为什么现在明星网红道歉大家都不信了？
+        </h3>
+        <div class="w-full h-fit mt-1 leading-4 flex justify-start justify-items-center text-[13px] text-regal-gray">
+          <span>
+            大力王
+          </span>
+          <span class="ml-1">
+            · 21小时前
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,12 +8,16 @@ export default {
     extend: {
       height: {
         '30': '7.5rem',
+        '43.5': '10.875rem',
         '45': '11.25rem',
         '62': '15.5rem',
       },
       spacing: {
         '35': '8.75rem',
-      }
+      },
+      colors: {
+        'regal-gray': '#9499A0'
+      },
     },
   },
   plugins: [],
