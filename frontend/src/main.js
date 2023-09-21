d0aa7614dc8b77a127967df04391d3a6624a0340
chenruipeng <1259903760@qq.com>
2023-09-21 17:58:51 +0800

    feat: 前端项目引入iconfont图标

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import axios from './utils/axios'
+import '../public/iconfont/iconfont.css'
 
 const app = createApp(App)
 app.config.globalProperties.$request = axios
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
   ],
   theme: {
     extend: {
+      width: {
+        '4.25': '1.0625rem',
+      },
       height: {
+        '4.25': '1.0625rem',
         '30': '7.5rem',
         '43.5': '10.875rem',
         '45': '11.25rem',
