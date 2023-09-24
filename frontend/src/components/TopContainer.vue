4d887261f049d9ee5107bbbf1630571f0051d3a9
chen ruipeng <1259903760@qq.com>
2023-09-25 05:28:13 +0800

    feat: 顶部搜索框部分样式

--- frontend/src/components/TopContainer.vue
+++ frontend/src/components/TopContainer.vue
@@ -52,9 +52,16 @@
       </ul>
 
       <!-- 搜索框 -->
-      <div class="flex-auto h-[38px] bg-white">
-        <div class="min-w-[181px] max-w-[500px]">
+      <div class="flex-auto h-[38px]">
+        <div class="relative my-0 mx-auto min-w-[181px] max-w-[500px]">
+          <form class="flex items-center pr-12 pl-1 relative z-[1] overflow-hidden leading-[38px] border border-solid border-[#E3E5E7] h-10 rounded-lg">
+            <div class="flex justify-between items-center relative px-2 w-full h-8 ">
 
+            </div>
+            <div class="absolute top-[3px] right-[7px] flex justify-center items-center m-0 p-0 w-8 h-8 rounded-md text-[#18191C] leading-8 cursor-pointer">
+              <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.3451 15.2003C16.6377 15.4915 16.4752 15.772 16.1934 16.0632C16.15 16.1279 16.0958 16.1818 16.0525 16.2249C15.7707 16.473 15.4456 16.624 15.1854 16.3652L11.6848 12.8815C10.4709 13.8198 8.97529 14.3267 7.44714 14.3267C3.62134 14.3267 0.5 11.2314 0.5 7.41337C0.5 3.60616 3.6105 0.5 7.44714 0.5C11.2729 0.5 14.3943 3.59538 14.3943 7.41337C14.3943 8.98802 13.8524 10.5087 12.8661 11.7383L16.3451 15.2003ZM2.13647 7.4026C2.13647 10.3146 4.52083 12.6766 7.43624 12.6766C10.3517 12.6766 12.736 10.3146 12.736 7.4026C12.736 4.49058 10.3517 2.1286 7.43624 2.1286C4.50999 2.1286 2.13647 4.50136 2.13647 7.4026Z" fill="currentColor"></path></svg>
+            </div>
+          </form>
         </div>
       </div>
 
