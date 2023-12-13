05ba3f2119ec6715bcb78ec852094af7a7e6374c
chenruipeng <chenruipeng@dongxinsoft.cn>
2023-12-13 16:37:09 +0800

    feat: 创建视频名称长度验证调整、视频时间相关信息调整

--- backend/middleware/validator/videoValidator.js
+++ backend/middleware/validator/videoValidator.js
@@ -9,7 +9,7 @@ const {
 module.exports.video = validate([
   body('title').notEmpty().withMessage('视频名不能为空').bail() // bail验证通过往下走 不通过就停止
   .isLength({
-    max: 30
-  }).withMessage('视频名长度不能大于30').bail(),
+    max: 50
+  }).withMessage('视频名长度不能大于50').bail(),
   // body('vodvideoId').notEmpty().withMessage('Vod不能为空').bail()
 ])
