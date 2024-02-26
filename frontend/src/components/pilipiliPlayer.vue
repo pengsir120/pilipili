b0d143659879a633c9b2b58b75aa192dbe4ba378
chenruipeng <1259903760@qq.com>
2024-02-26 17:47:57 +0800

    feat: 修复播放器点击进度条闪烁， 进度条指示器跟随进度移动

--- frontend/src/components/pilipiliPlayer.vue
+++ frontend/src/components/pilipiliPlayer.vue
@@ -3,7 +3,17 @@
     <div class="video-area relative flex-1 overflow-hidden bg-black">
       <div class="video-perch cursor-none flex w-full h-full justify-center relative">
         <div class="block w-full h-full box-border">
-          <video @timeupdate="handleTimeUpdate" @ended="isPaused = false" ref="video" autoplay crossorigin="anonymous" preload="auto" class="block w-full h-full m-auto" :src="props.options.url"></video>
+          <video
+            @timeupdate="handleTimeUpdate"
+            @ended="isPaused = false" 
+            muted="muted" 
+            ref="video" 
+            autoplay 
+            crossorigin="anonymous" 
+            preload="auto" 
+            class="block w-full h-full m-auto" 
+            :src="props.options.url">
+          </video>
         </div>
       </div>
       <div class="video-poster"></div>
@@ -31,7 +41,7 @@
                     </div>
                   </div>
                   <div class="w-full h-full absolute left-0 top-0 pointer-events-none"></div>
-                  <div class="w-5 h-5 pointer-events-none">
+                  <div :style="{transform: `translateX(${thumbOffsetX}px)`}" class="w-5 h-5 pointer-events-none">
                     <div class="h-full leading-3 scale-100 transition-all duration-200 w-full">
                       <div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" width="18" height="18" preserveAspectRatio="xMidYMid meet" style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px);"><defs><clipPath id="__lottie_element_55"><rect width="18" height="18" x="0" y="0"></rect></clipPath></defs><g clip-path="url(#__lottie_element_55)"><g transform="matrix(0.9883429408073425,-0.7275781631469727,0.6775955557823181,0.920446515083313,7.3224687576293945,-0.7606706619262695)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(0.9937776327133179,-0.11138220876455307,0.11138220876455307,0.9937776327133179,-2.5239999294281006,1.3849999904632568)"><path fill="rgb(51,51,51)" fill-opacity="1" d=" M0.75,-1.25 C0.75,-1.25 0.75,1.25 0.75,1.25 C0.75,1.663925051689148 0.4139249920845032,2 0,2 C0,2 0,2 0,2 C-0.4139249920845032,2 -0.75,1.663925051689148 -0.75,1.25 C-0.75,1.25 -0.75,-1.25 -0.75,-1.25 C-0.75,-1.663925051689148 -0.4139249920845032,-2 0,-2 C0,-2 0,-2 0,-2 C0.4139249920845032,-2 0.75,-1.663925051689148 0.75,-1.25z"></path></g></g><g transform="matrix(1.1436611413955688,0.7535901665687561,-0.6317168474197388,0.9587040543556213,16.0070743560791,2.902894973754883)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(0.992861807346344,0.1192704513669014,-0.1192704513669014,0.992861807346344,-2.5239999294281006,1.3849999904632568)"><path fill="rgb(51,51,51)" fill-opacity="1" d=" M0.75,-1.25 C0.75,-1.25 0.75,1.25 0.75,1.25 C0.75,1.663925051689148 0.4139249920845032,2 0,2 C0,2 0,2 0,2 C-0.4139249920845032,2 -0.75,1.663925051689148 -0.75,1.25 C-0.75,1.25 -0.75,-1.25 -0.75,-1.25 C-0.75,-1.663925051689148 -0.4139249920845032,-2 0,-2 C0,-2 0,-2 0,-2 C0.4139249920845032,-2 0.75,-1.663925051689148 0.75,-1.25z"></path></g></g><g transform="matrix(1,0,0,1,8.890999794006348,8.406000137329102)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,0.09099999815225601,1.1009999513626099)"><path fill="rgb(255,255,255)" fill-opacity="1" d=" M7,-3 C7,-3 7,3 7,3 C7,4.379749774932861 5.879749774932861,5.5 4.5,5.5 C4.5,5.5 -4.5,5.5 -4.5,5.5 C-5.879749774932861,5.5 -7,4.379749774932861 -7,3 C-7,3 -7,-3 -7,-3 C-7,-4.379749774932861 -5.879749774932861,-5.5 -4.5,-5.5 C-4.5,-5.5 4.5,-5.5 4.5,-5.5 C5.879749774932861,-5.5 7,-4.379749774932861 7,-3z"></path><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4" stroke="rgb(51,51,51)" stroke-opacity="1" stroke-width="1.5" d=" M7,-3 C7,-3 7,3 7,3 C7,4.379749774932861 5.879749774932861,5.5 4.5,5.5 C4.5,5.5 -4.5,5.5 -4.5,5.5 C-5.879749774932861,5.5 -7,4.379749774932861 -7,3 C-7,3 -7,-3 -7,-3 C-7,-4.379749774932861 -5.879749774932861,-5.5 -4.5,-5.5 C-4.5,-5.5 4.5,-5.5 4.5,-5.5 C5.879749774932861,-5.5 7,-4.379749774932861 7,-3z"></path></g></g><g transform="matrix(1,0,0,1,8.89900016784668,8.083999633789062)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,-2.5239999294281006,1.3849999904632568)"><path fill="rgb(51,51,51)" fill-opacity="1" d=" M0.875,-1.125 C0.875,-1.125 0.875,1.125 0.875,1.125 C0.875,1.607912540435791 0.48291251063346863,2 0,2 C0,2 0,2 0,2 C-0.48291251063346863,2 -0.875,1.607912540435791 -0.875,1.125 C-0.875,1.125 -0.875,-1.125 -0.875,-1.125 C-0.875,-1.607912540435791 -0.48291251063346863,-2 0,-2 C0,-2 0,-2 0,-2 C0.48291251063346863,-2 0.875,-1.607912540435791 0.875,-1.125z"></path></g></g><g transform="matrix(1,0,0,1,14.008999824523926,8.083999633789062)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,-2.5239999294281006,1.3849999904632568)"><path fill="rgb(51,51,51)" fill-opacity="1" d=" M0.8999999761581421,-1.100000023841858 C0.8999999761581421,-1.100000023841858 0.8999999761581421,1.100000023841858 0.8999999761581421,1.100000023841858 C0.8999999761581421,1.596709966659546 0.4967099726200104,2 0,2 C0,2 0,2 0,2 C-0.4967099726200104,2 -0.8999999761581421,1.596709966659546 -0.8999999761581421,1.100000023841858 C-0.8999999761581421,1.100000023841858 -0.8999999761581421,-1.100000023841858 -0.8999999761581421,-1.100000023841858 C-0.8999999761581421,-1.596709966659546 -0.4967099726200104,-2 0,-2 C0,-2 0,-2 0,-2 C0.4967099726200104,-2 0.8999999761581421,-1.596709966659546 0.8999999761581421,-1.100000023841858z"></path></g></g></g></svg>
@@ -271,24 +281,37 @@ const handlePlaypause = () => {
 }
 
 const currentTime = ref(0)
-const handleTimeUpdate = () => {
-  currentTime.value = video.value.currentTime
+const progressArea = ref()
+const progressPressDown = ref(false)
+const thumbOffsetX = ref(0)
+const handleTimeUpdate = (event) => {
+  if(!progressPressDown.value) {
+    const rect = progressArea.value.getBoundingClientRect()
+    thumbOffsetX.value = video.value.currentTime / props.options.duration * rect.width - 10
+    console.log();
+    currentTime.value = video.value.currentTime
+  }else {
+    return
+  }
 }
 
-const progressArea = ref()
+
 const handleProgressDown = (event) => {
-  console.log(progressArea.value.offsetWidth);
+  progressPressDown.value = true
   event.preventDefault()
   window.addEventListener("mousemove", progressMove)
   window.addEventListener("mouseup", progressUp)
   const rect = progressArea.value.getBoundingClientRect()
   const offsetX = event.clientX - rect.left
+  thumbOffsetX.value = offsetX - 10
+  console.log(thumbOffsetX.value);
   currentTime.value = offsetX / progressArea.value.clientWidth * props.options.duration
 }
 
 const progressMove = (event) => {
   const rect = progressArea.value.getBoundingClientRect()
   const offsetX = event.clientX - rect.left
+  thumbOffsetX.value = offsetX - 10
   const progressRate = offsetX / progressArea.value.clientWidth
   if(progressRate < 0) {
     currentTime.value = 0
@@ -305,6 +328,7 @@ const progressUp = (event) => {
   video.value.play()
   window.removeEventListener("mousemove", progressMove)
   window.removeEventListener("mouseup", progressUp)
+  progressPressDown.value = false
 }
 
 const isMuted = computed(() => {
