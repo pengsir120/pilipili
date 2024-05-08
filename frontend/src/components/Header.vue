2f245182878258e6360a4de2be9c19492842ac90
chenruipeng <chenruipeng@dongxinsoft.cn>
2024-05-08 17:53:31 +0800

    feat: 搜索历史静态框

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -55,7 +55,7 @@
       <div class="relative my-0 mx-auto min-w-[181px] max-w-[500px]">
         <form @submit.prevent class="flex items-center pr-12 pl-1 relative z-[1] overflow-hidden leading-[38px] border border-solid border-[#E3E5E7] h-10 rounded-lg bg-[#F1F2F3] opacity-90 transition-colors duration-300 hover:bg-white hover:opacity-100">
           <div class="flex justify-between items-center relative px-2 w-full h-8 border-2 border-solid border-transparent rounded-md">
-            <input v-model="searchVal" @keyup.enter="handleSearch" type="text" class="flex-1 overflow-hidden pr-2 bg-transparent text-[#61666D] text-[14px] leading-5 outline-0">
+            <input v-model="searchVal" @keyup.enter="handleSearch" type="text" @focus="handleInpFocus" class="flex-1 overflow-hidden pr-2 bg-transparent text-[#61666D] text-[14px] leading-5 outline-0">
             <div @click="searchVal = ''" :class="[searchVal.length ? 'visible' : 'invisible']" class="w-4 h-4 cursor-pointer">
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 14.75C11.7279 14.75 14.75 11.7279 14.75 8C14.75 4.27208 11.7279 1.25 8 1.25C4.27208 1.25 1.25 4.27208 1.25 8C1.25 11.7279 4.27208 14.75 8 14.75ZM9.64999 5.64303C9.84525 5.44777 10.1618 5.44777 10.3571 5.64303C10.5524 5.83829 10.5524 6.15487 10.3571 6.35014L8.70718 8.00005L10.3571 9.64997C10.5524 9.84523 10.5524 10.1618 10.3571 10.3571C10.1618 10.5523 9.84525 10.5523 9.64999 10.3571L8.00007 8.70716L6.35016 10.3571C6.15489 10.5523 5.83831 10.5523 5.64305 10.3571C5.44779 10.1618 5.44779 9.84523 5.64305 9.64997L7.29296 8.00005L5.64305 6.35014C5.44779 6.15487 5.44779 5.83829 5.64305 5.64303C5.83831 5.44777 6.15489 5.44777 6.35016 5.64303L8.00007 7.29294L9.64999 5.64303Z" fill="#C9CCD0"></path></svg>
             </div>
@@ -64,6 +64,42 @@
             <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.3451 15.2003C16.6377 15.4915 16.4752 15.772 16.1934 16.0632C16.15 16.1279 16.0958 16.1818 16.0525 16.2249C15.7707 16.473 15.4456 16.624 15.1854 16.3652L11.6848 12.8815C10.4709 13.8198 8.97529 14.3267 7.44714 14.3267C3.62134 14.3267 0.5 11.2314 0.5 7.41337C0.5 3.60616 3.6105 0.5 7.44714 0.5C11.2729 0.5 14.3943 3.59538 14.3943 7.41337C14.3943 8.98802 13.8524 10.5087 12.8661 11.7383L16.3451 15.2003ZM2.13647 7.4026C2.13647 10.3146 4.52083 12.6766 7.43624 12.6766C10.3517 12.6766 12.736 10.3146 12.736 7.4026C12.736 4.49058 10.3517 2.1286 7.43624 2.1286C4.50999 2.1286 2.13647 4.50136 2.13647 7.4026Z" fill="currentColor"></path></svg>
           </div>
         </form>
+        <div class="hidden w-full min-w-[236px] max-h-[612px] overflow-y-auto absolute bg-white border border-solid border-[#e6e9ee] box-border shadow-[0_2px_4px_0_rgba(0,0,0,0.1)] rounded-sm py-4 mt-0.5 text-[#212121]">
+          <div class="history box-border">
+            <div class="flex items-center justify-between px-4 box-border">
+              <div class="h-6 font-semibold text-[16px] leading-6">
+                搜索历史
+              </div>
+              <div class="text-[12px] leading-[15px] h-[15px] text-[#999999] cursor-pointer">
+                清空
+              </div>
+            </div>
+            <div class="max-h-[172px] px-4 overflow-hidden box-border">
+              <div class="flex flex-wrap mt-3 mr-[-10px] mb-1 box-border">
+                <div class="relative box-border h-[30px] pt-[7px] pb-[8px] px-2.5 text-[12px] leading-[15px] bg-[#f4f4f4] rounded mr-2.5 mb-2.5 cursor-pointer group hover:text-theme-color">
+                  <div class="whitespace-nowrap overflow-hidden text-ellipsis max-w-24 box-border">
+                    哥斯拉大战金刚2
+                  </div>
+                  <div class="group-hover:block hidden box-border absolute w-4 h-4 top-[-6px] right-[-6px] p-0.5">
+                    <svg data-v-340b780a="" class="fill-[#cccccc]" viewBox="0 0 1024 1024" width="14" height="14">
+                      <path data-v-340b780a="" d="M512 64.303538c-247.25636 0-447.696462 200.440102-447.696462 447.696462
+                        0 247.254314 200.440102 447.696462 447.696462 447.696462s447.696462-200.440102
+                        447.696462-447.696462S759.25636 64.303538 512 64.303538zM710.491727 665.266709c12.491499
+                        12.491499 12.489452 32.729425-0.002047 45.220924-6.246261 6.246261-14.429641 9.370415-22.611997
+                        9.370415s-16.363689-3.121084-22.60995-9.366322L512 557.222971 358.730221 710.491727
+                        c-6.246261 6.246261-14.429641 9.366322-22.611997 9.366322s-16.365736-3.125177-22.611997-9.370415
+                        c-12.491499-12.491499-12.491499-32.729425 0-45.220924l153.268756-153.266709L313.50725 358.730221
+                        c-12.491499-12.491499-12.489452-32.729425 0.002047-45.220924s32.729425-12.495592 45.220924-0.004093
+                        l153.268756 153.268756 153.268756-153.268756c12.491499-12.491499 32.729425-12.487406 45.220924
+                        0.004093s12.493545 32.729425 0.002047 45.220924L557.225017 512 710.491727 665.266709z">
+                      </path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -285,38 +321,11 @@ const uploadVideo = ref(null)
 
 const handleUploadVideo = () => {
   uploadVideo.value.show()
-  // const input = document.createElement("input")
-  // input.setAttribute("type", "file")
-  // input.setAttribute("accept", "video/*")
-  // input.onchange = async (event) => {
-  //   const file = event.target.files[0]
-  //   const video = document.createElement("video")
-  //   video.preload = 'metadata'
-  //   video.src = URL.createObjectURL(file)
-  //   video.onloadedmetadata = () => {
-  //     URL.revokeObjectURL(video.src)
-  //     const formData = new FormData()
-  //     formData.append("file", file)
-  //     formData.append("metaData", JSON.stringify({
-  //       duration: getVideoTime(video.duration)
-  //     }))
-  //     $request({
-  //       url: "/video/upload",
-  //       method: "post",
-  //       data: formData,
-  //       headers: {
-  //         'Content-Type': 'multipart/form-data'
-  //       }
-  //     })
-  //   }
-  // }
-  // input.click()
-  // input.remove()
 }
 
-
-
-onMounted(() => {
-  // uploadVideo.value.show()
-})
+const historySearch = ref([])
+const inpFocus = ref(false)
+const handleInpFocus = () => {
+  inpFocus.value = true
+}
 </script>
